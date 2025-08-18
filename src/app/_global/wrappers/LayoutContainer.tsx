26a15f3ff13058206d08b63a515c8d7fd2201e47
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-08-18 18:23:56 +0900

    save

--- src/app/_global/wrappers/LayoutContainer.tsx
+++ src/app/_global/wrappers/LayoutContainer.tsx
@@ -2,8 +2,8 @@
 import { usePathname } from 'next/navigation'
 import useUser from '../hooks/useUser'
 import loadable from '@loadable/component'
-const AdminHeader = loadable(() => import('../outlines/admin/Header'))
-const AdminSide = loadable(() => import('../outlines/admin/Side'))
+const AdminHeader = loadable(() => import('../admin/Header'))
+const AdminSide = loadable(() => import('../admin/Side'))
 
 const Header = loadable(() => import('../outlines/Header'))
 const Footer = loadable(() => import('../outlines/Footer'))
